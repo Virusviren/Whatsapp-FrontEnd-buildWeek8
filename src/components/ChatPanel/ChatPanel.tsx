7d515412c33c1ecac0f28970b42e9242996550f7
Tiago Brandao <tiagofpbrandao@gmail.com>
2021-09-08 10:17:54 +0200

    Adds search bar and message input

--- src/components/ChatPanel/ChatPanel.tsx
+++ src/components/ChatPanel/ChatPanel.tsx
@@ -1,3 +1,6 @@
+import { Form, FormControl } from "react-bootstrap"
+import { MdInsertEmoticon } from "react-icons/md"
+import { GrAttachment } from "react-icons/gr"
 import Avatar from "../Avatar/Avatar"
 
 import "./ChatPanel.css"
@@ -10,13 +13,20 @@ interface ChatPanelProps {
 
 const ChatPanel = ({ title, participants, avatar }: ChatPanelProps) => {
   return (
-    <div className="ChatPanel">
+    <div className="ChatPanel d-flex flex-column justify-content-between">
       <div className="d-flex align-items-center topbar">
         <Avatar url={avatar} />
         <div>
           <h6>{title}</h6>
           <p className="text-muted">{participants.join(", ")}</p>
         </div>
+      </div>
+      <div className="input-bar d-flex align-items-center">
+        <MdInsertEmoticon />
+        <GrAttachment />
+        <Form className="d-flex p-2 w-100">
+          <FormControl type="text" placeholder="Type a message" aria-label="Type a message" className="rounded-pill" />
+        </Form>
       </div>
     </div>
   )
