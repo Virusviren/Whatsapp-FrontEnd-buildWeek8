71e254e6785c0c084136e75662039fcf0bc2257b
Viren Patil <virenpatil1@outlook.com>
2021-09-08 16:05:49 +0200

    OffCanvas created

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,5 @@
-
 import Login from '../Login/Login';
- import Dashboard from "../Dashboard/Dashboard"
+import Dashboard from '../Dashboard/Dashboard';
 import Registration from '../Registration/Registration';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
@@ -8,14 +7,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 const App = () => {
   return (
     <div className='App'>
-      <Router>
+      {/* <Router>
         <Route exact path='/' component={Login} />
         <Route exact path='/registration' component={Registration} />
-      </Router>
+      </Router> */}
       <Dashboard />
     </div>
   );
 };
-
 
 export default App;
